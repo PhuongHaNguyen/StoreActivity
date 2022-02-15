a507e6f8d6d2ea81c33c22bffcbe10026158ad90
PhuongHaNguyen <nguyenha17196@gmail.com>
2022-02-15 14:38:26 +0700

    xóa source ko xài tới

--- StoreCore/ConfigHelper.cs
+++ StoreCore/ConfigHelper.cs
@@ -8,28 +8,6 @@ namespace StoreCore
 {
     public static class ConfigHelper
     {
-        //public static string connectionString = string.Empty;
-        //public static string serverNameConn = string.Empty;
-        //public static string userNameConn = string.Empty;
-        //public static string passwordConn = string.Empty;
-        //public static string tableStoreConfig = string.Empty;
-
-        //public static string etpBaseUrl = string.Empty;
-        //public static string valueEtpBaseUrl = string.Empty;
-
-        //public static string etpApiPing = string.Empty;
-        //public static string valueEtpApiPing = string.Empty;
-
-        //public static string etpApiGetLastVersion = string.Empty;
-        //public static string valueEtpApiGetLastVersion = string.Empty;
-
-        //public static string storeId = string.Empty;
-        //public static string valueStoreId = string.Empty;
-
-        //public static string storeCode = string.Empty;
-        //public static string valuestoreCode = string.Empty;
-
-        //public static string storeVersion = string.Empty;
-        //public static string valueStoreVersion = string.Empty;
+        
     }
 }
